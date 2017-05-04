671133a30bfdf13ffb5e9e5ed3e12b94851dae05
cramja <marc.spehlmann@gmail.com>
2017-05-03 23:44:11 -0500

    heatmap

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ Database = function() {
     this.base_students = data.students;
   };
 
-  this.AssignmentScoresSorted = function() {
+  this.AssignmentScores = function() {
     // For each assignment, get all the students grades.
     // returns: {assignment#:[grade,grade...]}
     var assn_scores = {};
@@ -42,22 +42,22 @@ Database = function() {
     this.base_students.forEach(function(s) {
       grades.push(s.grades);
     });
-    var max_assn = -1;
     grades.forEach(function(g){
       g.forEach(function(s, j) {
         map_push(assn_scores, j, s.score);
-        max_assn = j > max_assn ? j : max_assn;
       });
     });
     for (var i = 0; i <= max_assn; i++) {
-      assn_scores[i] = assn_scores[i].sort();
+      assn_scores[i] = assn_scores[i];
     }
     return assn_scores;
   };
 
   this.AssnScores2Box = function() {
-    // returns [{label:"assn_name", values:[v0,v1,...,vn]}]
-    var scores = this.AssignmentScoresSorted();
+    // returns [{label:"assn_name", values:[{name:, value:},]}]
+    var scores = this.AssignmentScores();
+    var assn_names = this.base_assignments.map(function(d){return d.name;});
+
     var box_data = [];
     for (var key in scores) {
       var sc = scores[key];
@@ -67,6 +67,21 @@ Database = function() {
       });
     }
     return box_data;
+  };
+
+  this.GetStudentsScores = function() {
+    // returns [{name, id, [{name, score}]}]
+    var assn_names = this.base_assignments.map(function(d){return d.name;});
+    var student_score = this.base_students.map(function(d){
+      return {
+        id: d.id,
+        name: d.sortable_name,
+        values: d.grades.map(function(d,i) {
+          return {name:assn_names[i], value:d.score};
+        })
+      }
+    });
+    return student_score;
   };
 
   this.GetStudents = function() {
@@ -164,6 +179,121 @@ BoxPlotView = function() {
   };
 };
 
+(function() {
+  d3.HeatMap = function () {
+
+    var height = 1,
+      width = 1,
+      domain = [];
+
+    function HeatMap(g) {
+      var xoffset = width * 0.1,
+        yoffset = height * 0.1;
+
+      var y_sc = d3.scaleBand()
+        .domain(d3.range(g.data().length))
+        .range([0, height - yoffset])
+        .padding(0.1);
+
+      g.each(function(d,i) {
+        // d == {label, values}
+        var g = d3.select(this),
+          n = d.values.length,
+          idx = i;
+
+        var x_sc = d3.scaleBand()
+          .domain(d.values.map(function(d){ return d.name; }))
+          .range([0, width - xoffset])
+          .padding(0.1);
+
+        var c_sc = d3.scaleLinear()
+          .domain(domain)
+          .range(['white', 'green']);
+
+        // offset the row to the proper location
+        var grow = g.append("g")
+          .attr("transform", function(d,i) { return "translate(" + xoffset + "," + y_sc(idx) + ")"; });
+
+        // Do highlight of row
+        grow.selectAll("rect.hl")
+          .data([d.selected])
+          .enter()
+          .append("rect")
+            .attr("class", "hl")
+            .attr("width", width)
+            .attr("height", y_sc.bandwidth() * 1.1)
+            .attr("dy", - (y_sc.bandwidth() * 0.05))
+            .style("visibility", function(d) {return d ? "visible" : "hidden"; });
+
+        // Add/update tiles
+        var gtile  = grow.selectAll("rect.tile")
+          .data(d.values);
+        gtile.enter()
+            .append("rect")
+            .attr("class", "tile")
+            .attr("x", function(d) {
+              return x_sc(d.name);
+            })
+            .attr("height", y_sc.bandwidth())
+            .attr("width", x_sc.bandwidth())
+            .attr("fill", function(d) { return c_sc(d.value);});
+
+        // y labels
+        var glabel = g.append("g");
+        glabel.selectAll("text.label")
+          .data([d])
+            .enter()
+            .append("text")
+            .attr("class", "label")
+            .attr("x", xoffset * 0.9)
+            .attr("y", function(d) {return y_sc(idx); } )
+            .attr("dy", y_sc.bandwidth() * 0.8)
+            .attr("text-anchor", "end")
+            .text(function(d) {return "" + d.id;});
+      });
+      // x labels
+      var assn_data = g.data()[0].values.map(function(d){ return d.name; });
+      var x_sc = d3.scaleBand()
+        .domain(assn_data)
+        .range([0, width - xoffset])
+        .padding(0.1);
+
+      var gylabel = g.append("g")
+        .attr("transform",
+          "translate(" + xoffset + "," + (height-yoffset) + ")");
+      gylabel.selectAll("text.label")
+        .data(assn_data)
+        .enter()
+        .append("text")
+        .attr("class", "label")
+        .attr("x", function(d) {return x_sc(d) + x_sc.bandwidth()/4;})
+        .attr("y", yoffset - yoffset*.75)
+        .attr("transform", function(d) {return "rotate(30, " + (x_sc(d) + x_sc.bandwidth()/4)+ "," + (yoffset - yoffset*0.75) +")"})
+        .text(function(d){return d;});
+    }
+
+    HeatMap.height = function(y) {
+      if (!arguments.length) return height;
+      height = y;
+      return HeatMap;
+    };
+
+    HeatMap.width = function(x) {
+      if (!arguments.length) return width;
+      width = x;
+      return HeatMap;
+    };
+
+    HeatMap.domain = function(x) {
+      if(!arguments.length) return domain;
+      domain = x;
+      return HeatMap;
+    };
+
+    return HeatMap;
+  };
+})();
+
 EntityNavView = function() {
   // Creates a navigation view using jquery.
   //
@@ -177,14 +307,47 @@ EntityNavView = function() {
 Controller = function() {
   this.init = function(db) {
     this.db = db;
-    this.viewer = new BoxPlotView();
-    this.viewer.init(this.db);
+    this.height = 62;
+    this.width = 100;
 
-    $("#nav-container").append("<ul></ul>");
-    var students = this.db.GetStudents();
-    for (var i = 0; i < students.length; i++) {
-      $("#nav-container>ul").append("<li>" + students[i].name + "</li>");
-    }
+    // bind data to an svg, create a heatmap
+    var dataset = this.db.GetStudentsScores();
+    dataset.forEach(function(d) {d.selected=false;});
+    dataset[1].selected = true;
+
+    var domain = [0, 70]; // todo !hack
+
+    var heatmap = d3.HeatMap()
+      .width(this.width)
+      .height(this.height)
+      .domain(domain);
+
+    var heatmapg = d3.select("#main-view-container")
+      .append("svg")
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("viewBox", "0 0 " + this.width + " " + this.height)
+      .selectAll("g")
+        .data(dataset)
+        .enter()
+          .append("g")
+          .attr("class", "heatmap")
+          .call(heatmap);
+
+    var list = d3.select("#nav-container")
+      .append("ul")
+      .selectAll("li")
+      .data(this.db.GetStudents());
+    list.enter()
+      .append("li")
+      .text(function(d) {return d.name;})
+      .on("click", function(d) {
+        d.selected = !d.selected;
+        dataset[5].selected = true;
+        console.log(dataset);
+        d3.select("svg")
+          .selectAll("g")
+          .data(dataset).call(heatmap);
+      });
   }
 };
 
